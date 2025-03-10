b9dbb1e0c00ed23601d22fa338de15d2960e3c74
ZenithSUS <jeranmerino147@gmail.com>
2025-03-10 10:53:56 +0800

    Add status field to updateRequirementRenewal function

--- controllers/requirementcontroller.js
+++ controllers/requirementcontroller.js
@@ -114,7 +114,7 @@ export const updateRequirementRenewal = async (req, res) => {
     }
 
     await updateDocumentRenewal(
-      { expiration: newExpiration, dateSubmitted: renewal, renewal: renewal },
+      { expiration: newExpiration, dateSubmitted: renewal, renewal: renewal, status: "Active" },
       requirementId
     );
 
