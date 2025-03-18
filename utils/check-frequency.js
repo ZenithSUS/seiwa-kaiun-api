70f64328cae3fd5a4a41e8002a4ac9fde5569662
ZenithSUS <jeranmerino147@gmail.com>
2025-03-18 10:58:24 +0800

    Add new data in requirement

--- utils/check-frequency.js
+++ utils/check-frequency.js
@@ -2,7 +2,7 @@ export const checkFrequency = (status, remainingDays, frequency) => {
     const annualFrequencies = ["Annual", "Semi Annual", "2 Years", "3 Years", "4 Years", "5 Years"];
 
     return (
-        (status !== "Expired" &&
+        (status === "Active" &&
             ((remainingDays <= 15 && frequency === "Monthly") ||
                 (remainingDays <= 90 && annualFrequencies.includes(frequency)) ||
                 (remainingDays <= 30 && frequency === "Quarterly"))) ||
